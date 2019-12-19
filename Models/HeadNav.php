f073c21e75f0f96db8309f0e8dfa0b6073a88b9c
Kellybp <indeterminatarum@gmail.com>
2019-12-19 12:52:54 -0500

    project restart with React

--- Models/HeadNav.php
+++
@@ -1,3 +0,0 @@
-<div id="headNav">
-	<p>Squirrel O Vision</p>
-</div>
--- Views/videosEdit.php
+++
@@ -1,6 +0,0 @@
-<li class="ui-state-default hovering" style="color:white;" ><?php echo($file); ?>
-<input type="hidden" value="<?php echo($file); ?>">
-    <div style="float: right;">
-      <!--<input type="checkbox" name="ch<?php //echo($subCounter); $subCounter++; ?>" value="<?php //echo($file); ?>">-->
-    </div>
-</li>
